df2891be5a98298f2d1e9460cbf69fb73f51d934
bhavintandel <bhautandel@yahoo.com.com>
2016-03-16 09:41:06 +0000

    Implemented constructor

--- src/domParser/Employee.java
+++ src/domParser/Employee.java
@@ -7,12 +7,19 @@ package domParser;
  */
 
 public class Employee {
-	private int ID;
+	private String ID;
 	private String firstName;
 	private String lastName;
 	private int age;
-	private int salary;
+	private double salary;
 	
+	public Employee(String ID, String firstName, String lastName, int age, double salary){
+		this.ID = ID;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.salary = salary;
+	}
 	@Override
 	public String toString(){
 		return "ID: " + String.valueOf(ID) + "\nName: " + firstName + " " + lastName 
